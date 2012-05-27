617ffa10af3d44669ce4f6d433b98368bbaff377
Jessie Taylor <b3457m0d3@interr0bang.net>
2012-05-27 11:43:48 -0400

    countdown

--- application/views/countdown_head.php
+++ application/views/countdown_head.php
@@ -17,16 +17,24 @@
       }
     </style>
 <?php
-$time = $this->uri->segment(3);
-if(!$time) $time = "01:12:12:00";
+if($this->uri->segment(3)) $time = $this->uri->segment(3);
+else $time = "2012-09-22";
+$_seconds = strtotime($time)-time();
+$days = round($_seconds / 86400);
+if($days<10) $days = "0".$days;
+$hours = round(($_seconds % 86400) / 3600);
+if($hours<10) $hours = "0".$hours;
+$minutes = round((($_seconds % 86400) % 3600) / 60);
+if($minutes<10) $minutes = "0".$minutes;
+$until = $days.":".$hours.":".$minutes.":00";
 ?>
 <script src="/assets/js/jquery.countdown.js"></script>
-
 <script type="text/javascript">
+
 $(function(){
-$('#counter').countdown({
-  image: '/assets/img/digits.png',
-  startTime: '<?=$time?>'
-});
+      $('#counter').countdown({
+        image: 'http://www.falcontether.cc/assets/img/digits.png',
+        startTime: '<?php echo $until; ?>'
+      });
 });
 </script>
